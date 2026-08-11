c61e7de1a981ba5c6015abbdc8ff5a527724e42c
cadharmender86 <80145514+cadharmender86@users.noreply.github.com>
2026-08-12 03:15:15 +0530

    Register internal test deposit API

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,39 +1,15 @@
 from fastapi import APIRouter
 
-from app.api.v1 import assets, health, users
-from app.api.v1 import auth
-from app.api.v1 import assets
-from app.api.v1 import accounts
-from app.api.v1 import ledger
-from app.api.v1 import transfers
+from app.api.v1 import accounts, assets, auth, health, internal, ledger, transfers, users
 
 
 api_router = APIRouter()
 
-api_router.include_router(
-    health.router,
-)
-
-api_router.include_router(
-    users.router,
-)
-
-api_router.include_router(
-    assets.router,
-)
-
-api_router.include_router(
-    auth.router,
-)
-
-api_router.include_router(
-    accounts.router
-)
-
-api_router.include_router(
-    ledger.router
-)
-
-api_router.include_router(
-    transfers.router
-)
+api_router.include_router(health.router)
+api_router.include_router(users.router)
+api_router.include_router(assets.router)
+api_router.include_router(auth.router)
+api_router.include_router(accounts.router)
+api_router.include_router(ledger.router)
+api_router.include_router(transfers.router)
+api_router.include_router(internal.router)
